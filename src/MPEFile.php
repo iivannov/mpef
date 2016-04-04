d85cdb195cc3762fd7581602c2eebadfc84f3266
iivannov <iivannov@gmail.com>
2016-04-04 15:13:02 +0300

    phpDoc

--- src/MPEFile.php
+++ src/MPEFile.php
@@ -6,22 +6,62 @@ namespace Iivannov\MPEF;
 class MPEFile
 {
 
+    /**
+     * Encryption key - a random 32 character string
+     * used to encrypt and decrypt data
+     *
+     * @var string
+     */
     protected $key;
 
+    /**
+     * Encryption cipher used
+
+     * @var string
+     */
     protected $cipher;
 
+    /**
+     * Instance of an Encryption provider
+     *
+     * @var EncrypterContract
+     */
     protected $encrypter;
 
+    /**
+     * The file path of the current file
+     *
+     * @var string
+     */
     protected $path;
 
+    /**
+     * The body of the file
+     *
+     * @var string
+     */
     protected $data;
 
+    /**
+     * The extension of the original file that was encrypted
+     *
+     * @var string
+     */
     protected $originalExtension;
 
+    /**
+     * The MIME type of the original file that was encrypted
+     *
+     * @var string
+     */
     protected $originalMimeType;
 
-    protected $originalDataType;
 
+    /**
+     * MPEFile constructor.
+     * @param null $key
+     * @param string $cipher
+     */
     public function __construct($key = null, $cipher = 'AES-256-CBC')
     {
         $this->key = $key;
@@ -30,12 +70,33 @@ class MPEFile
         $this->setEncrypter();
     }
 
-    //*
+    /**
+     * Reads the data stored in the encrypted file
+     *
+     * @return string
+     */
+    public function read()
+    {
+        $data = json_decode($this->encrypter->decrypt($this->data));
+
+        return base64_decode($data->data);
+    }
+
+    /**
+     * Encrypts the content and store it
+     *
+     * @param $content
+     */
     public function write($content)
     {
         $this->data = $this->encrypter->encrypt($this->prepare($content));
     }
 
+    /**
+     * Encrypts the contents of a given file
+     *
+     * @param $path
+     */
     public function file($path)
     {
         $this->originalMimeType = mime_content_type($path);
@@ -44,6 +105,13 @@ class MPEFile
         $this->write(file_get_contents($path));
     }
 
+    /**
+     * Saves the file to a given location
+     *
+     * @param null $path
+     * @return int
+     * @throws \Exception
+     */
     public function save($path = null)
     {
 
@@ -58,13 +126,13 @@ class MPEFile
     }
 
 
-    public function read()
-    {
-        $data = json_decode($this->encrypter->decrypt($this->data));
-
-        return base64_decode($data->data);
-    }
-
+    /**
+     * Formats the data and all the additional info
+     * before encrypting it
+     *
+     * @param $content
+     * @return string
+     */
     private function prepare($content)
     {
         return json_encode([
@@ -75,6 +143,15 @@ class MPEFile
     }
 
 
+    /**
+     * Loads the contents of a MPEF file
+     *
+     * @param $path
+     * @param $key
+     * @param string $cipher
+     * @return MPEFile
+     * @throws \Exception
+     */
     public static function load($path, $key, $cipher = 'AES-256-CBC')
     {
 
@@ -90,6 +167,10 @@ class MPEFile
         return $instance;
     }
 
+    /**
+     * Setter for the Encryption provider
+     * @param EncrypterContract|null $encrypter
+     */
     protected function setEncrypter(EncrypterContract $encrypter = null)
     {
         if (null != $encrypter) {
@@ -99,6 +180,11 @@ class MPEFile
         }
     }
 
+    /**
+     * Returns the encryption key for the file
+     *
+     * @return string
+     */
     protected function key()
     {
         if (null == $this->key) {
@@ -108,6 +194,12 @@ class MPEFile
         return $this->key;
     }
 
+    /**
+     * Helper method to write the file in the filesystem
+     *
+     * @param $path
+     * @return int
+     */
     private function writeDataToFile($path)
     {
         $dir = pathinfo($path, PATHINFO_DIRNAME);
